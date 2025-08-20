91ae1e4457ba10ac3df4b10f29850652e7622a7d
Minh Chung <di35hef@login01.sng.lrz.de>
2025-08-20 10:36:01 +0200

    temporarily add experimental design

---
+++ experiments/affinity_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set_mmcso_thread_affinity()
+{
+	echo Placing MMCSO Threads in $1 MPI processes on each $2th Core of each Node with total $3 Cores
+	a=$(for i in $(seq 0 $(($1 - 1))); do echo $i:$(( ((($i+1)*$2) - 1) % $3 )); done)
+	export MMCSO_THREAD_AFFINITY=$(echo $a | sed -e 's/\s\+/,/g')
+}
+
+set_mmcso_thread_affinity $1 $2 $3
+env | grep MMCSO
+
