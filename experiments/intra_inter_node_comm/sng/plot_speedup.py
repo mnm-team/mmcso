ebf52e0c89662f6364c5ef9111a77e474ffce418
ctminh <minh.chung@lrz.de>
2025-08-24 21:55:05 +0200

    added plots for micro-bench on SNG and HAWK

--- experiments/intra_inter_node_comm/sng/plot_speedup.py
+++ experiments/intra_inter_node_comm/sng/plot_speedup.py
@@ -44,32 +44,25 @@ def get_rank(filename):
         print(filename + ": No match found")
         exit()
     
-def make_dataframe(csv_files, conf, mem):
-    if mem:
-        conf = conf + '-mem-'
-        
-    csv_files = [f for f in csv_files if conf in f]
+def make_dataframe(csv_files):
     dfs = []
-    
     for f in csv_files:
         df = pd.read_csv(f)
-        rank = get_rank(f)
-        offload = False if '-default' in f else True
-        df['rank'] = rank
+        offload = False if '_native' in f else True
         df['offload'] = offload
         dfs = dfs + [df]
-        
-    return pd.concat(dfs, ignore_index=True)
+    df_concat = pd.concat(dfs, ignore_index=True)
+    # print(df_concat)
+    return df_concat
 
 def plot_speedup(df, ax, title, is_mem):
+
     ax.hlines(y=1.0, xmin=df['msg_size'].min(), xmax=df['msg_size'].max(), color='darkgrey')
-    
     df_offload = df['offload'] == True
     
     # merge dfs to compute speedup
-    df = pd.merge(df.loc[~df_offload], df.loc[df_offload], on=('msg_size', 'work'), suffixes=('_default', '_offload'))
-    df['speedup'] = df['comm_time_default'] / df['comm_time_offload']
-    print(df)
+    df = pd.merge(df.loc[~df_offload], df.loc[df_offload], on=('msg_size', 'work'), suffixes=('_native', '_offload'))
+    df['speedup'] = df['comm_time_native'] / df['comm_time_offload']
     
     if is_mem:
         df = df[(df['work'] == 500) | (df['work'] == 5000) | (df['work'] == 50000) | (df['work'] == 500000)]
@@ -79,8 +72,6 @@ def plot_speedup(df, ax, title, is_mem):
     dfg = df.groupby(['work'])
     
     for i, ((w), gp) in enumerate(dfg):
-        # print(f"Work {w} ns has {len(gp)} points")
-        # print(gp[['msg_size', 'speedup']])
         line = gp.plot(ax=ax, x='msg_size', y='speedup', logx=True, logy=False, color=colors[i], linestyle='-.' if is_mem else ':', 
                        marker='^' if is_mem else 'o', markerfacecolor='none', markersize=8)
                        #, yerr='std', elinewidth=0.1, capsize=3, capthick=0.1)
@@ -91,7 +82,7 @@ def plot_speedup(df, ax, title, is_mem):
         legend_labels = [f"{10*work//1000} kbytes" for work in dfg.groups]
         ax.set_ylabel('Speedup (memcpy)')
     else:
-        ax.set_title(title)
+        # ax.set_title(title)
         legend_labels = [f"{work} ns" for work in dfg.groups]
         ax.set_ylabel('Speedup (sleep)')
         
@@ -105,52 +96,55 @@ def plot_speedup(df, ax, title, is_mem):
                                  title=f"Work ({'memcpy' if is_mem else 'sleep'})"
                                  )
     
-def plot(axs, is_mem):
+def plot(axs, df, is_mem):
     
     conf1 = ('2-4', '1 MPI proc per Socket, 63 threads per MPI proc')
     conf2 = ('2-8', '2 MPI proc per Socket, 31 threads per MPI proc')
     conf3 = ('2-16', '1 MPI proc per NUMA domain, 15 threads per MPI proc')
     
-    configs = [conf1, conf3]
+    configs = [conf1]
     
-    ymax = 11.5
+    ymax = 3.5
     # ymax = 17.5
     ytickmax = math.ceil(ymax)
     
     # data frames for non-memory-intensive work
     for i, (conf, title) in enumerate(configs):
-        df = make_dataframe(csv_files, conf, is_mem)
         # df = df[(df['work'] == 100) | (df['work'] == 1000) | (df['work'] == 10000) | (df['work'] == 100000)]
-        print(f"Dataframe for config {conf} has {len(df)} rows")
         df = df.groupby(['nthreads', 'msg_size', 'offload', 'work']).mean().drop(['rank', 'thread'], axis=1).reset_index()
-        # print(df)
-        plot_speedup(df, axs[i], title, is_mem)
+        plot_speedup(df, axs, title, is_mem)
     
-    for ax in axs:
-        ax.grid(alpha=0.3)
-        ax.set_xlim(2, 1.0e9)
-        ax.set_xlabel('Message Size [bytes]')
-        # ax.set_ylabel('speedup')
-        ax.set_yticks(range(ytickmax))
-        ax.set_ylim(0.5, ymax)
-        # ax.legend().set_visible(False)
+    axs.grid(alpha=0.3)
+    axs.set_xlim(2, 1.0e9)
+    axs.set_xlabel('Message Size [bytes]')
+    # axs.set_ylabel('speedup')
+    axs.set_yticks(range(ytickmax))
+    axs.set_ylim(0.5, ymax)
+    # axs.legend().set_visible(False)
         
-    axs[1].legend().set_visible(False)
+    # axs[1].legend().set_visible(False)
 
 if __name__ == '__main__':
 
-    fig, axs = plt.subplots(2, 2, figsize=(16, 9), sharex=True, sharey=True)
+    # fig, axs = plt.subplots(2, 1, figsize=(16, 9), sharex=True, sharey=True)
+    fig, axs = plt.subplots(2, 1, figsize=(8, 9), sharex=True, sharey=True)
 
-    csv_files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.csv')]
-    # print(csv_files)
+    # csv_files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.csv')]
+    csv_files_compute = ['./result/inter_native_compute_2nodes_4procs.csv', 
+                         './result/inter_offload_compute_2nodes_4procs.csv']
+    csv_files_mem = ['./result/inter_native_mem_2nodes_4procs.csv', 
+                     './result/inter_offload_mem_2nodes_4procs.csv']
+    config = ('2-4', '1 MPI proc per Socket, 23 threads per MPI proc')
+    df_compute = make_dataframe(csv_files_compute)
+    df_mem = make_dataframe(csv_files_mem)
+    plot(axs[0], df_compute, False)
+    plot(axs[1], df_mem, True)
     
-    plot(axs[0], False)
-    plot(axs[1], True)
-    
-    fig.suptitle(f'Hawk: AMD Epyc 7702 Dual-Socket 128 Cores (2 Nodes)')
+    fig.suptitle(f'SNG: Intel Xeon(R) Gold 6148 48 Cores (2 Nodes)')
     
     plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=0.5)
     plt.subplots_adjust(top=0.90)
-    plt.savefig(f'./speedup_microbench_hawk.pdf')
+    plt.savefig(f'./speedup_microbench_sng.pdf')
+    print("Saved figure to ./speedup_microbench_sng.pdf")
     
     # plt.show()
