133b43b8b38c43c7044abda58fd500a606459319
Sergej Breiter <sergej.breiter@nm.ifi.lmu.de>
2025-08-27 10:33:37 +0200

    add sng hpgmgfv build and config scripts

---
+++ experiments/spechpc2021hpgmgfv/sng/scripts/build-sng.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+benchmarks="534.hpgmgfv_t 634.hpgmgfv_s 734.hpgmgfv_m"
+
+for b in $benchmarks; do
+	runhpc -c sng-funneled --define model=omp --define label=sng-funneled --action=build
+	runhpc -c sng-multiple --define model=omp --define label=sng-multiple --action=build
+done
+
+
