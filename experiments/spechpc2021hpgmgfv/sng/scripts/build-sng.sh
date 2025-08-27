b59a9a0f1b23e007b218a8d957e675c5b92c130b
Minh Chung <di35hef@login01.sng.lrz.de>
2025-08-27 15:15:55 +0200

    added initial results of spechpc on sng

--- experiments/spechpc2021hpgmgfv/sng/scripts/build-sng.sh
+++ experiments/spechpc2021hpgmgfv/sng/scripts/build-sng.sh
@@ -3,8 +3,8 @@
 benchmarks="534.hpgmgfv_t 634.hpgmgfv_s 734.hpgmgfv_m"
 
 for b in $benchmarks; do
-	runhpc -c sng-funneled --define model=omp --define label=sng-funneled --action=build
-	runhpc -c sng-multiple --define model=omp --define label=sng-multiple --action=build
+	runhpc -c sng-funneled --define model=omp --define label=sng-funneled --action=build $b
+	runhpc -c sng-multiple --define model=omp --define label=sng-multiple --action=build $b
 done
 
 
