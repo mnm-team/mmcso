41d18a888d64c5d4a9c0e5ff01b03d84fd6e53a1
Sergej Breiter <sergej.breiter@nm.ifi.lmu.de>
2025-12-14 16:47:51 +0100

    Code cleanup

--- src/print_sizes.cpp
+++
@@ -1,6 +0,0 @@
-#if 0
-    #include <cstdio>
-    #include "offload.h"
-    printf("sizeof(MPICommand): %zu\n", sizeof(MPICommand));
-    printf("sizeof(OffloadCommand): %zu\n", sizeof(mmcso::OffloadCommand));
-#endif
